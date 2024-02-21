dcf907e4413e06c4bb1dd2816c15f12bdad40345
Jiapeng Zhao <logan@gmail.com>
2024-02-21 10:27:07 -0800

    modified:   carla_gym.Dockerfile 	modified:   test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import carla_gym
 
-env = carla_gym.env(**kwargs)           # AEC API
-#env = carla_gym.parallel_env(**args)  # Parallel API
+#env = carla_gym.env(**kwargs)           # AEC API
+if __name__ == "__main__":
+    env = carla_gym.parallel_env(**args)  # Parallel API
